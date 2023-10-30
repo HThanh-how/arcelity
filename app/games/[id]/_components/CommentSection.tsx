2026dbe92c0d1001ee15f2ad673e0ef123a89102
AZURE231 <95262095+AZURE231@users.noreply.github.com>
2023-10-30 20:13:08 +0700

    update comment section

--- app/games/[id]/_components/CommentSection.tsx
+++ app/games/[id]/_components/CommentSection.tsx
@@ -1,4 +1,4 @@
-import { Card, Text } from "@chakra-ui/react";
+import { Card, Divider, Text } from "@chakra-ui/react";
 import React from "react";
 
 export default function CommentSection() {
@@ -8,7 +8,15 @@ export default function CommentSection() {
         Ratings & Reviews
       </Text>
       <Card p={10} w={"full"} bg={"whiteAlpha.300"} textColor={"white"}>
-        <Text>Comments</Text>
+        <Text>Game Informer</Text>
+        <Divider my={3} textColor={"whiteAlpha.600"} />
+        <Text mb={3}>7.5/10</Text>
+        <Text textColor={"whiteAlpha.600"}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+          adipisci at rem maiores natus a eos, magnam, ad repudiandae inventore
+          aspernatur modi voluptates magni nemo eligendi? Totam maiores
+          accusamus itaque.
+        </Text>
       </Card>
     </>
   );
