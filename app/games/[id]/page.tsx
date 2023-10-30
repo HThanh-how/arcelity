95d2329db4973507a9aebe02f4be7105014cf376
AZURE231 <95262095+AZURE231@users.noreply.github.com>
2023-10-30 15:37:49 +0700

    add skeleton when loading

--- app/games/[id]/page.tsx
+++ app/games/[id]/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { Container, Heading, SimpleGrid, GridItem } from "@chakra-ui/react";
+import {
+  Container,
+  Heading,
+  SimpleGrid,
+  GridItem,
+  Skeleton,
+} from "@chakra-ui/react";
 import React, { use } from "react";
 import GameImages from "./_components/GameImages";
 import BuyGame from "./_components/BuyGame";
@@ -13,34 +19,39 @@ export default function GamePage({ params }: { params: { id: string } }) {
   return (
     <Container p={0} maxW={"75%"} my={10}>
       {isError && <div>Something went wrong ...</div>}
-      {isLoading ? (
-        <div>Loading ...</div>
-      ) : (
-        <>
-          <Heading mb={5}>{data.name}</Heading>
-          <SimpleGrid columns={{ sm: 1, md: 1, lg: 4 }} spacing={0}>
-            <GridItem colSpan={3}>
-              <GameImages />
-            </GridItem>
-            <GridItem colSpan={1}>
-              <BuyGame
-                price={data.price}
-                releaseDate={data.releaseDate}
-                developer={data.developer}
-                genres={data.genres}
-                saleDetails={data.saleDetails}
-              />
-            </GridItem>
-            <GridItem colSpan={3}>
-              <GameDescription
-                description={data.description}
-                systemRequirements={data.systemRequirements}
-              />
-              <CommentSection />
-            </GridItem>
-          </SimpleGrid>
-        </>
-      )}
+
+      <Skeleton isLoaded={!isLoading}>
+        <Heading mb={5}>{data.name}</Heading>
+      </Skeleton>
+      <SimpleGrid columns={{ sm: 1, md: 1, lg: 4 }} spacing={0}>
+        <GridItem colSpan={3}>
+          <Skeleton isLoaded={!isLoading}>
+            <GameImages />
+          </Skeleton>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Skeleton isLoaded={!isLoading}>
+            <BuyGame
+              price={data.price}
+              releaseDate={data.releaseDate}
+              developer={data.developer}
+              genres={data.genres}
+              saleDetails={data.saleDetails}
+            />
+          </Skeleton>
+        </GridItem>
+        <GridItem colSpan={3}>
+          <Skeleton isLoaded={!isLoading}>
+            <GameDescription
+              description={data.description}
+              systemRequirements={data.systemRequirements}
+            />
+          </Skeleton>
+          <Skeleton isLoaded={!isLoading}>
+            <CommentSection />
+          </Skeleton>
+        </GridItem>
+      </SimpleGrid>
     </Container>
   );
 }
