a9dfc1b3dba5a85726017cf1e29f8bf54b4dd8ff
HThanh-how <thanhlt1235@gmail.com>
2023-10-31 21:01:26 +0700

    up test access token

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -45,9 +45,12 @@ const handleLogin = async () => {
     );
 
     const { access_token, id } = response.data;
-
     localStorage.setItem("access_token", access_token);
+    console.log(access_token);
+    const createdAt = new Date().toISOString();
+    document.cookie = `access_token=${access_token}; expires=Thu, 01 Jan 2022 00:00:00 UTC; path=/`;
     router.push("/");
+    
   } catch (error) {
     toast({
       title: `Username or Password incorrect`,
